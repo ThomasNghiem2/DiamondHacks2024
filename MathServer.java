86281cb72e89f4f4cfe53a50a5125b0dc83f12e1
ThomasNghiem2 <tnghiem@ucsd.edu>
2024-04-07 00:01:42 -0700

    Added path options

--- MathServer.java
+++ MathServer.java
@@ -14,22 +14,47 @@ class MathServer {
 
     public String operation(URI url) {
         int range = 20;
-        if (url.getPath().equals("/add")) {
-            String[]
-            while (true) {
-                int num1 = (int)(Math.random() * range);
-                int num2 = (int)(Math.random() * range);
+        int min = 1;
+        Integer solution = 0;
 
-            }
+        //expect /add
+        if (url.getPath().equals("/add")) {
+            int num1 = (int)(Math.random() * range) + min;
+            int num2 = (int)(Math.random() * range) + min;
+            solution = num1 + num2;
+            
         }
         if (url.getPath().equals("/subtract")) {
-
+            int num1 = (int)(Math.random() * range) + min;
+            int num2 = (int)(Math.random() * range) + min;
+            solution = num2 - num1;;
         }
         if (url.getPath().equals("/multiply")) {
-
+            int num1 = (int)(Math.random() * range) + min;
+            int num2 = (int)(Math.random() * range) + min;
+            solution = num1 * num2;
         }
         if (url.getPath().equals("/divide")) {
-
+            int num1 = (int)(Math.random() * range) + min;
+            int num2 = (int)(Math.random() * range) + min;
+            //fix due to the rounding down of java
+            solution = num1 / num2;
+        }
+        //expect /solve?answer=<answer>
+        if (url.getPath().equals("/solve")) {
+            String[] params = url.getQuery().split("=");
+            String answer = params[1];
+            if (params[0].equals("answer")) {
+                if (answer == solution.toString()) {
+                    history += "Correct!\n\n";
+                }
+                else {
+                    history += "Oopsie, thats incorrect. Try again!";
+                }
+            }
+            else {
+                return "Invalid parameters: " + String.join("=", params);
+            }
         }
     }
 }
