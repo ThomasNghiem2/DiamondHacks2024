8620f42d15beb1ed169eb1c515d8962c75be2b24
ThomasNghiem2 <tnghiem@ucsd.edu>
2024-04-07 00:43:02 -0700

    added main

--- MathServer.java
+++ MathServer.java
@@ -1,60 +1,71 @@
 import java.io.IOException;
 import java.net.URI;
-import java.util.HashMap;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 
-class MathServer {
+class Calcuation {
     String history = "Welcome! To choose the topic you want to practice, type the corresponding path into the url: \"/add\", \"/subtract\", \"/multiply\", \"/divide\" \n\n";
-    HashMap<String, String> dataAdd = new HashMap<String, String>();
-    HashMap<String, String> dataSubtract = new HashMap<String, String>();
-    HashMap<String, String> dataMultiply = new HashMap<String, String>();
-    HashMap<String, String> dataDivide = new HashMap<String, String>();
+    Integer solution;
 
     public String operation(URI url) {
         int range = 20;
         int min = 1;
-        Integer solution = 0;
 
         //expect /add
         if (url.getPath().equals("/add")) {
             int num1 = (int)(Math.random() * range) + min;
             int num2 = (int)(Math.random() * range) + min;
             solution = num1 + num2;
-            
+            history += num1 + " + " + num2 + " = ?\n";
+            return history;
         }
-        if (url.getPath().equals("/subtract")) {
+        else if (url.getPath().equals("/subtract")) {
             int num1 = (int)(Math.random() * range) + min;
             int num2 = (int)(Math.random() * range) + min;
-            solution = num2 - num1;;
+            solution = num2 - num1;
+            history += num2 + " - " + num1 + " = ?\n";
+            return history;
         }
-        if (url.getPath().equals("/multiply")) {
+        else if (url.getPath().equals("/multiply")) {
             int num1 = (int)(Math.random() * range) + min;
             int num2 = (int)(Math.random() * range) + min;
             solution = num1 * num2;
+            history += num1 + " * " + num2 + " = ?\n";
+            return history;
         }
-        if (url.getPath().equals("/divide")) {
+        else if (url.getPath().equals("/divide")) {
             int num1 = (int)(Math.random() * range) + min;
             int num2 = (int)(Math.random() * range) + min;
             //fix due to the rounding down of java
             solution = num1 / num2;
+            history += num1 + " / " + num2 + " = ?\n";
+            return history;
         }
         //expect /solve?answer=<answer>
-        if (url.getPath().equals("/solve")) {
+        else if (url.getPath().equals("/solve")) {
             String[] params = url.getQuery().split("=");
             String answer = params[1];
             if (params[0].equals("answer")) {
-                if (answer == solution.toString()) {
+                if (answer.equals(solution.toString())) {
                     history += "Correct!\n\n";
                 }
                 else {
-                    history += "Oopsie, thats incorrect. Try again!";
+                    history += "Oopsie, thats incorrect. Try again!\n";
                 }
+                return history;
             }
             else {
                 return "Invalid parameters: " + String.join("=", params);
             }
         }
+        return history;
     }
 }
+
+class MathServer {
+    public static void main(String[] args) throws IOException {
+      int port = Integer.parseInt(args[0]);
+      Server.start(port, new Calcuation());
+    }
+  }
